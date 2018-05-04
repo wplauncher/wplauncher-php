4a3eabc6e60627aa67958b7809cfe04c3aeb15ba
Ben Shadle <benshadle@gmail.com>
2018-05-03 22:36:04 -0700

    add stripe connect

--- src/StripeConnect.php
+++ src/StripeConnect.php
@@ -20,78 +20,27 @@ class StripeConnect extends WplauncherClient
 	    return json_decode($response->getBody(), true);
 	}
 	/**
-	 * Returns a specific instance
+	 * May not need this because it's easier to just throw stripe directly to the api.wplauncher.com/v1/connect/stripe-callback 
+	 * Returns Stripe Connect information after connecting the user to the WPLauncher Stripe Account
 	 *
-	 * @param int $id
+	 * @param array $request
 	 *
 	 * @return mixed
-	 */
-	public function getInstance($id) {
-	    if (!is_numeric($id)) {
-	        throw new \InvalidArgumentException('The instance id must be numeric.');
+	 *
+	public function getCallback($request) {
+		/*
+            'state' => 'required|exists:users,stripe_connect_state|string',
+			'scope' => 'required|string',
+			'code' => 'required|string',
+		*
+		
+	    if (!is_array($request)) {
+	        throw new \InvalidArgumentException('The request var must be an array.');
 	    }
-	    $response = $this->client->get('instances/' . $id);
+	    $response = $this->client->get('connect/stripe-callback' . $request);
 	    $this->checkResponseStatusCode($response, 200);
 	    return json_decode($response->getBody(), true);
 	}
-	/**
-	 * Creates a new Plan
-	 *=
-	 * @param array $instance
-	 *
-	 * @return mixed
-	 */
-	public function createInstance($instance = []) {
-		/*
-		    'name' => 'required|string',
-			'instance_type_id' => 'required|exists:instance_types,id|integer',
-			'price' => 'bail|required|integer',
-			'interval' => 'bail|required|exists:intervals,name|string',
-			'interval_count' => 'bail|required|integer',
-			'currency' => 'bail|required|string',
-			'trial_period_days' => 'integer',
-			'environment' => 'string'
-		*
-		if (!is_numeric($user_id)) {
-     	    throw new \InvalidArgumentException('The user id must be numeric.');
-     	}*/
-		
-		$response = $this->client->post('instance', ['body' => json_encode($instance)]);
-	    $this->checkResponseStatusCode($response, 201);
-	    return json_decode($response->getBody());
-	}
-	/**
-	 * Return all the instances of a given user
-	 *
-	 * @param int $user_id
-	 *
-	 * @return array()
-	 */
-	 public function getUserInstances($user_id) {
-	     if (!is_numeric($user_id)) {
-	         throw new \InvalidArgumentException('The user id must be numeric.');
-	     }
-	     $response = $this->client->get('instances', ['query' => ['user_id' => $user_id]]);
-	     $this->checkResponseStatusCode($response, 200);
-	     return json_decode($response->getBody());
-	 }
-	 
-	/**
-	 * Updates a instance
-	 *
-	 * @param array $instance
-	 * @return mixed
-	 */
-	public function updateInstance($instance) {
-	    if (!is_numeric($instance['id'])) {
-	        throw new \InvalidArgumentException('The instance id must be numeric.');
-	    } 
-		if (!is_array($instance)) {
-	        throw new \InvalidArgumentException('The instance must be an array.');
-	    }
-	    $response = $this->client->patch('instances/' . $instance['id'], ['body' => json_encode($instance)]);
-	    $this->checkResponseStatusCode($response, 200);
-	    return json_decode($response->getBody());
-	}
+	*/
 	
 }
