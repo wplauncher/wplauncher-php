6e8280ab6d13d60f874cfb22c3266eec256b5b92
Ben Shadle <benshadle@gmail.com>
2018-05-03 22:55:57 -0700

    update stripe connect

--- src/StripeConnect.php
+++ src/StripeConnect.php
@@ -20,27 +20,7 @@ class StripeConnect extends WplauncherClient
 	    return json_decode($response->getBody(), true);
 	}
 	/**
-	 * May not need this because it's easier to just throw stripe directly to the api.wplauncher.com/v1/connect/stripe-callback 
-	 * Returns Stripe Connect information after connecting the user to the WPLauncher Stripe Account
-	 *
-	 * @param array $request
-	 *
-	 * @return mixed
-	 *
-	public function getCallback($request) {
-		/*
-            'state' => 'required|exists:users,stripe_connect_state|string',
-			'scope' => 'required|string',
-			'code' => 'required|string',
-		*
-		
-	    if (!is_array($request)) {
-	        throw new \InvalidArgumentException('The request var must be an array.');
-	    }
-	    $response = $this->client->get('connect/stripe-callback' . $request);
-	    $this->checkResponseStatusCode($response, 200);
-	    return json_decode($response->getBody(), true);
-	}
-	*/
+	 * May not need stripe callback because it's easier to just throw stripe directly to the api.wplauncher.com/v1/connect/stripe-callback 
+    */
 	
 }
