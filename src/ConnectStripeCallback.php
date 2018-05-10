8e454a1f198069b812e2ec10c69be14bfa4b336e
Ben Shadle <benshadle@gmail.com>
2018-05-09 18:55:05 -0700

    add connect stripe callback

---
+++ src/ConnectStripeCallback.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Wplauncher;
+
+use Wplauncher\Wplauncher;
+
+class ConnectStripeCallback extends Wplauncher
+{
+	
+    /**
+     * Returns a specific object
+     *
+     * @param int $id
+     * @param array $opts
+     *
+     * @return mixed
+     */
+    public static function retrieve($id = '', $opts = null)
+    {
+		return self::_retrieve($id, $opts);
+    }
+    /**
+     * May not need stripe callback because it's easier to just throw stripe directly to the api.wplauncher.com/v1/connect/stripe-callback
+    */
+}
